4c8ac4fcf782afec75a132ccfbdcb5e1569f91f2
tkaneko0204 <takehiro.kaneko@ctc-g.co.jp>
2016-05-02 12:19:49 +0900

    'モックアップを作成する'まで完了

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,41 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
+var stylus = require('gulp-stylus');
 var browserSync = require('browser-sync').create();
 
-gulp.task('browser-sync', ['jade'], function() {
+gulp.task('browser-sync', ['app', 'jade', 'stylus'], function() {
   browserSync.init({
     server: {
       baseDir: './dist',
       routes: {
-        '/bower_components': 'bower_components',
-        '/app.js': 'app.js'
+        '/bower_components': 'bower_components'
       }
     }
   });
 
-  gulp.watch('./**/*.jade', ['jade-watch']);
+  gulp.watch('./src/app.js', ['app'], browserSync.reload);
+  gulp.watch('./src/*.jade', ['jade'], browserSync.reload);
+  gulp.watch('./src/stylus/*.stylus', ['stylus'], browserSync.reload);
 });
 
-gulp.task('jade-watch', ['jade'], browserSync.reload);
+gulp.task('app', function() {
+  gulp.src('./src/app.js')
+    .pipe(gulp.dest('./dist'));
+});
 
 gulp.task('jade', function() {
-  return gulp.src('./**/*.jade')
+  return gulp.src('./src/*.jade')
     .pipe(jade({
       pretty: true
     }))
     .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('stylus', function() {
+  return gulp.src('./src/stylus/*.stylus')
+    .pipe(stylus())
+    .pipe(gulp.dest('./dist/css'))
     .pipe(browserSync.stream());
 });
 
