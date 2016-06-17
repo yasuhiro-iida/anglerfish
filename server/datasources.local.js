9f571ebbbd48a6ec779806d7380fa5fd3b180bac
tkaneko0204 <takehiro.kaneko@ctc-g.co.jp>
2016-06-17 14:13:11 +0900

    標準のmysqlサービスを使用する

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -1,5 +1,9 @@
 module.exports = {
   mysqlDs: {
-    host: process.env.DB_HOST || '192.168.99.100'
+    host: process.env.DB_HOST || '192.168.99.100',
+    port: process.env.DB_PORT || 3306,
+    database: process.env.DB_DATABASE || "ebdb",
+    username: process.env.DB_USERNAME || "anglerfish",
+    password: process.env.DB_PASSWORD || "^anglerfish$"
   }
 }
