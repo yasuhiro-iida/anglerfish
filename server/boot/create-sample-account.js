5044088e5339a096c93372401fc1d72c26aaaf16
tkaneko0204 <takehiro.kaneko@ctc-g.co.jp>
2016-06-21 14:54:01 +0900

    マイグレーションスクリプトのproduction対応

--- server/boot/create-sample-account.js
+++ server/boot/create-sample-account.js
@@ -1,49 +1,66 @@
 var async = require('async');
 
 module.exports = function(app) {
-  ds = app.dataSources.mysqlDs;
+  var ds = app.dataSources.mysqlDs;
+  var fns;
 
-  async.waterfall([
-      createAccounts,
-      createTodos
-  ], function(err, result) {
+  if (process.env.NODE_ENV === 'production') {
+    fns = [update];
+  } else {
+    fns = [migrate, createAccounts, createTodos];
+  }
+
+  async.waterfall(fns, function(err, result) {
     if (err) console.log('Error: ' + err);
     console.log('DB connection Success!');
   });
 
-  function createAccounts(callback) {
-    ds.automigrate(['Account', 'AccessToken', 'ACL', 'RoleMapping', 'Role'], function(err) {
-      if (err) return callback(err);
+  function update(callback) {
+    ds.isActual(function(err, actual) {
+      if (err) callback(err);
 
-      var accounts = [
-        {
-          email: 'foo@foo.com',
-          password: 'foo'
-        },
-        {
-          email: 'bar@bar.com',
-          password: 'bar'
-        },
-        {
-          email: 'baz@baz.com',
-          password: 'baz'
-        }
-      ];
-
-      app.models.Account.create(accounts, callback);
+      if (!actual) {
+        ds.autoupdate(function(err) {
+          if (err) callback(err);
+        });
+      }
+      console.log('Model is synced. Migration not needed.')
+      callback();
     });
   }
 
-  function createTodos(accounts, callback) {
-    ds.automigrate('ToDo', function(err) {
-      if (err) return callback(err);
-
-      var todos = [];
-      for (var i = 0, len = accounts.length; i < len; i++) {
-        todos.push(createSampleTodo(accounts[i]));
-      }
-      app.models.ToDo.create(todos, callback);
+  function migrate(callback) {
+    ds.automigrate(function(err) {
+      if (err) callback(err);
+      callback();
     });
+  }
+
+  function createAccounts(callback) {
+    var accounts = [
+      {
+        email: 'foo@foo.com',
+        password: 'foo'
+      },
+      {
+        email: 'bar@bar.com',
+        password: 'bar'
+      },
+      {
+        email: 'baz@baz.com',
+        password: 'baz'
+      }
+    ];
+
+    app.models.Account.create(accounts, callback);
+  }
+
+  function createTodos(accounts, callback) {
+    var todos = [];
+    for (var i = 0, len = accounts.length; i < len; i++) {
+      todos.push(createSampleTodo(accounts[i]));
+    }
+    app.models.ToDo.create(todos, callback);
 
     function createSampleTodo(account) {
       var name = account.email.split('@')[0];
